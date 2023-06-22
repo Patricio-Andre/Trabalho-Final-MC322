896794e8c539040c3529b4e05704a4c116afce9a
Maria Beatriz Guimaraes T. M. Moreira <mariabeatrizmoreira20@gmail.com>
2023-06-22 15:12:46 -0300

    adicionando de novo as classes e corrigindo coisas

---
+++ HospHub/src/HospHub/Enfermeire.java
@@ -0,0 +1,40 @@
+package HospHub;
+
+import java.time.LocalTime;
+
+public class Enfermeire extends  ProfissionalSaude {
+    public Enfermeire(String nome, String registro, String endereco, LocalTime inicioplantao,
+        LocalTime fimplantao, String cPF, int limitepacientes){
+            super(nome, registro, endereco, inicioplantao, fimplantao, cPF, limitepacientes);
+        }
+
+
+    public boolean cadastrar(Object pacienteo) throws ProfissionalUnfitException{
+        ProfissionalUnfitException e = new ProfissionalUnfitException();
+        if(this.getAtendendo() < this.getLimitepacientes() && LocalTime.now().isAfter(this.getInicioplantao()) && LocalTime.now().isBefore(this.getFimplantao())){ // ver se to de plantao e se minha especialidade eh a dele;
+            if(pacienteo instanceof Paciente){ //tipo medico so que sem especializacao
+            Paciente paciente = (Paciente) pacienteo;
+                if(this.getMapaPacientes().containsKey(paciente.getCPF())){
+                System.out.println("paciente ja cadastrado");
+                return false;
+            }
+            this.getMapaPacientes().put(paciente.getCPF(), paciente);
+            this.setAtendendo(this.getAtendendo()+1);
+            System.out.println("paciente cadastrado com sucesso");
+            return true;
+            }
+            }
+        throw e;
+    }
+
+    public String alimentar(Paciente paciente){
+        return " o paciente " + paciente + " foi alimentado ";
+    }
+
+
+    public String darBanho(Paciente paciente){
+        return "o paciente " + paciente + " foi banhado ";
+    }
+
+    
+}
