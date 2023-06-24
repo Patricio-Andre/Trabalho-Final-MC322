66b07de7ee78ae11c7f0a88ca18ca626aceeea39
FernandoSacerdote <f240807@g.unicamp.br>
2023-06-24 19:41:45 -0300

    exceptions

--- HospHub/src/HospHub/ExameIncompativelException.java
+++ HospHub/src/HospHub/ExameIncompativelException.java
@@ -1,5 +1,19 @@
 package HospHub;
 
-public class ExameIncompativelException extends Exception{
+public class ExameIncompativelException extends Exception {
+    public ExameIncompativelException() {
+        super("Exame inconpatível com o paciente");
+    }
 
+    public ExameIncompativelException(String message) {
+        super(message);
+    }
+
+    public ExameIncompativelException(Throwable cause) {
+        super(cause);
+    }
+
+    public ExameIncompativelException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
--- HospHub/src/HospHub/ProfissionalUnfitException.java
+++ HospHub/src/HospHub/ProfissionalUnfitException.java
@@ -1,5 +1,19 @@
 package HospHub;
 
-public class ProfissionalUnfitException extends Exception{
+public class ProfissionalUnfitException extends Exception {
+    public ProfissionalUnfitException() {
+        super("Profissional indisponível");
+    }
 
+    public ProfissionalUnfitException(String message) {
+        super(message);
+    }
+
+    public ProfissionalUnfitException(Throwable cause) {
+        super(cause);
+    }
+
+    public ProfissionalUnfitException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
