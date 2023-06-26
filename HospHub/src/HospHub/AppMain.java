b1d5248c56beb607ee486eb440e9fa923c7fb6d4
FernandoSacerdote <f240807@g.unicamp.br>
2023-06-26 03:01:36 -0300

    menu - receitas e listar

--- HospHub/src/HospHub/AppMain.java
+++ HospHub/src/HospHub/AppMain.java
@@ -1,5 +1,174 @@
 package HospHub;
 
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class AppMain {
 
+    public static void cadastrarReceita(Scanner in, Hospital hospital) {
+        System.out.println("Digite o nome do remedio");
+        String nome = in.nextLine();
+        boolean nomeEhValido = ValidaDados.validaNome(nome);
+        while (!nomeEhValido) {
+            System.out.println("nome invalido, tente novamente");
+            nome = in.nextLine();
+            nomeEhValido = ValidaDados.validaNome(nome);
+        }
+        System.out.println("Informe a dosagem");
+        String dosagem = in.nextLine();
+        System.out.println("Informe o registro do profissional da saude");
+        String registro = in.nextLine();
+        System.out.println("Informe o cpf do paciente");
+        String cpf = in.nextLine();
+        ProfissionalSaude profissional = hospital.buscaProfissionalSaude(registro);
+        profissional.gerarReceita(nome, dosagem, profissional, cpf);
+    }
+
+    public static void cadastrarReceitaAntibiotico(Scanner in, Hospital hospital) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        System.out.println("Digite o nome do remedio");
+        String nome = in.nextLine();
+        boolean nomeEhValido = ValidaDados.validaNome(nome);
+        while (!nomeEhValido) {
+            System.out.println("nome invalido, tente novamente");
+            nome = in.nextLine();
+            nomeEhValido = ValidaDados.validaNome(nome);
+        }
+        System.out.println("Informe a dosagem");
+        String dosagem = in.nextLine();
+        System.out.println("Informe a data de vencimento da receita");
+        String data = in.nextLine();
+        boolean dataEhValida = ValidaDados.validaData(data);
+        while (!dataEhValida) {
+            System.out.println("data invalida, tente novamente");
+            data = in.nextLine();
+            dataEhValida = ValidaDados.validaData(data);
+        }
+        LocalDate dataFormatada = LocalDate.parse(data, formatter);
+        System.out.println("Informe o registro do profissional da saude");
+        String registro = in.nextLine();
+        System.out.println("Informe o cpf do paciente");
+        String cpf = in.nextLine();
+        ProfissionalSaude profissional = hospital.buscaProfissionalSaude(registro);
+        profissional.gerarReceita(registro, dosagem, profissional, dataFormatada, cpf);
+    }
+
+    public static void removerReceita(Scanner in, Hospital hospital) {
+        System.out.println("Digite o nome do remedio");
+        String nome = in.nextLine();
+        boolean nomeEhValido = ValidaDados.validaNome(nome);
+        while (!nomeEhValido) {
+            System.out.println("nome invalido, tente novamente");
+            nome = in.nextLine();
+            nomeEhValido = ValidaDados.validaNome(nome);
+        }
+        System.out.println("Informe o registro do profissional da saude");
+        String registro = in.nextLine();
+        System.out.println("Informe o cpf do paciente");
+        String cpf = in.nextLine();
+        ProfissionalSaude profissional = hospital.buscaProfissionalSaude(registro);
+        if (profissional.removerReceita(nome, cpf)) {
+            System.out.println("Receita removida com sucesso");
+        } else {
+            System.out.println("Falha ao realizar a remoção da receita");
+        }
+    }
+
+    public static void imprimeMenu() {
+        System.out.println("Bem vindo ao menu");
+        System.out.println("Selecione uma das seguintes ações:");
+        System.out.println("1 - receitas");
+        System.out.println("2 - listar");
+        System.out.println("0 - sair");
+    }
+
+    public static void imprimeMenuListar() {
+        System.out.println("Selecione o item que deseja listar");
+        System.out.println("1 - Funcionarios");
+        System.out.println("2 - Medicos");
+        System.out.println("3 - Enfermeiros");
+        System.out.println("4 - Internacoes");
+        System.out.println("0 - voltar");
+    }
+
+    public static void menuListar(Scanner in, Hospital hospital) {
+        imprimeMenuListar();
+        int comando = in.nextInt();
+        switch (comando) {
+            case 1:
+                System.out.println(hospital.ListarFuncionarios());
+                break;
+            case 2:
+                System.out.println(hospital.ListarMedicos());
+                break;
+            case 3:
+                System.out.println(hospital.ListarEnfermeiros());
+                break;
+            case 4:
+                System.out.println(hospital.ListarInternacoes());
+                break;
+            case 0:
+                break;
+            default:
+                System.out.println("Comando invalido");
+                break;
+        }
+    }
+
+    public static void imprimeMenuReceitas() {
+        System.out.println("Selecione a ação desejada");
+        System.out.println("1 - gerar receita normal");
+        System.out.println("2 - gerar receita de antibiótico");
+        System.out.println("3 - remover receita");
+        System.out.println("0 - voltar");
+    }
+
+    public static void menuReceitas(Scanner in, Hospital hospital) {
+        imprimeMenuReceitas();
+        int comando = in.nextInt();
+        switch (comando) {
+            case 1:
+                cadastrarReceita(in, hospital);
+                break;
+            case 2:
+                cadastrarReceitaAntibiotico(in, hospital);
+                break;
+            case 3:
+                break;
+            case 0:
+                break;
+            default:
+                System.out.println("Comando invalido");
+                break;
+        }
+    }
+
+    public static void main(String args[]) {
+        // new MinhaInterfaceGrafica();
+        Hospital hospital = new Hospital("Novo Hospital", "12.821.660/0001-12", "(19)992926745",
+                "hosp@placeholder.com.br", "Rua temporaria", null);
+        Scanner entrada = new Scanner(System.in);
+        imprimeMenu();
+        int comando = entrada.nextInt();
+        while (comando != 0) {
+            switch (comando) {
+                case 1:
+                    menuReceitas(entrada, hospital);
+                    break;
+                case 2:
+                    menuListar(entrada, hospital);
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Comando invalido tente novamente");
+                    break;
+            }
+            imprimeMenu();
+            comando = entrada.nextInt();
+        }
+        System.out.println("saiu");
+        entrada.close();
+    }
 }
--- HospHub/src/HospHub/Receita.java
+++ HospHub/src/HospHub/Receita.java
@@ -1,4 +1,5 @@
 package HospHub;
+
 import java.util.LinkedList;
 import java.time.LocalDate;
 import java.lang.StringBuilder;
@@ -7,10 +8,10 @@ public class Receita {
     private String remedio; // nome do composto quimico -> vai ser unico pq a anvisa garante isso
     private String dosagem; // tem que ser String pq tem unidade
     private ProfissionalSaude profissionalSaude;
-    private LinkedList <String> listaInterferencias;
-    private LocalDate dataVencimento; // so vale pra antibiotico 
+    private LinkedList<String> listaInterferencias;
+    private LocalDate dataVencimento; // so vale pra antibiotico
 
-    public Receita(String remedio, String dosagem, ProfissionalSaude profissionalSaude){
+    public Receita(String remedio, String dosagem, ProfissionalSaude profissionalSaude) {
         this.remedio = remedio;
         this.dosagem = dosagem;
         this.profissionalSaude = profissionalSaude;
@@ -18,7 +19,7 @@ public class Receita {
         dataVencimento = null;
     }
 
-    public Receita(String remedio, String dosagem, ProfissionalSaude profissionalSaude, LocalDate dataVencimento){
+    public Receita(String remedio, String dosagem, ProfissionalSaude profissionalSaude, LocalDate dataVencimento) {
         this.remedio = remedio;
         this.dosagem = dosagem;
         this.profissionalSaude = profissionalSaude;
@@ -26,40 +27,48 @@ public class Receita {
         this.dataVencimento = dataVencimento;
     }
 
-
     public String getRemedio() {
         return remedio;
     }
+
     public void setRemedio(String remedio) {
         this.remedio = remedio;
     }
+
     public String getDosagem() {
         return dosagem;
     }
+
     public void setDosagem(String dosagem) {
         this.dosagem = dosagem;
     }
+
     public ProfissionalSaude getProfissionalSaude() {
         return profissionalSaude;
     }
+
     public void setProfissionalSaude(ProfissionalSaude profissionalSaude) {
         this.profissionalSaude = profissionalSaude;
     }
+
     public LinkedList<String> getListaInterferencias() {
         return listaInterferencias;
     }
+
     public void setListaInterferencias(LinkedList<String> listaInterferencias) {
         this.listaInterferencias = listaInterferencias;
     }
+
     public LocalDate getDataVencimento() {
         return dataVencimento;
     }
+
     public void setDataVencimento(LocalDate dataVencimento) {
         this.dataVencimento = dataVencimento;
     }
 
-    public boolean addInterferencia(String interferencia){
-        if(listaInterferencias.contains(interferencia)){
+    public boolean addInterferencia(String interferencia) {
+        if (listaInterferencias.contains(interferencia)) {
             System.out.println("interferencia ja cadastrada");
             return false;
         }
@@ -69,32 +78,33 @@ public class Receita {
 
     }
 
-    public boolean removeInterferencia(String inter){
-        if(!listaInterferencias.contains(inter)){
+    public boolean removeInterferencia(String inter) {
+        if (!listaInterferencias.contains(inter)) {
             System.out.println("interferencia nao cadastrada"); // poderia dar exception mas ja tem 3
             return true;
         }
         listaInterferencias.remove(inter);
         System.out.println("interferencia removida com sucesso");
         return true;
-    
+
     }
 
-    public String listarInterferencias(){
+    public String listarInterferencias() {
         StringBuilder sb = new StringBuilder();
-        for(String value : listaInterferencias){
+        for (String value : listaInterferencias) {
             sb.append(value);
         }
-       return sb.toString().equals("") ? "nao ha interferencias" : sb.toString();
+        return sb.toString().equals("") ? "nao ha interferencias" : sb.toString();
     }
 
-    public String toString(){
-        try{
-        return " o remedio " + remedio + " de dose " + dosagem + " receitado por " + profissionalSaude + " interfere com " + listarInterferencias() + " essa receita eh valida" +
-        " ateh " + dataVencimento;
-        }
-        catch (NullPointerException e){
-            return " o remedio " + remedio + " de dose " + dosagem + " receitado por " + profissionalSaude + " interfere com " + listarInterferencias();
+    public String toString() {
+        try {
+            return " o remedio " + remedio + " de dose " + dosagem + " receitado por " + profissionalSaude
+                    + " interfere com " + listarInterferencias() + " essa receita eh valida" +
+                    " ateh " + dataVencimento;
+        } catch (NullPointerException e) {
+            return " o remedio " + remedio + " de dose " + dosagem + " receitado por " + profissionalSaude
+                    + " interfere com " + listarInterferencias();
         }
     }
 }
