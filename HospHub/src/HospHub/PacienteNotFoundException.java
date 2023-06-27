f14a1602c3134947cbd7596b9693152a5530b10a
D-nss <127433077+D-nss@users.noreply.github.com>
2023-06-27 01:08:35 -0300

    Update PacienteNotFoundException.java

--- HospHub/src/HospHub/PacienteNotFoundException.java
+++ HospHub/src/HospHub/PacienteNotFoundException.java
@@ -1,5 +1,19 @@
 package HospHub;
 
-public class PacienteNotFoundException extends Exception{
+public class PacienteNotFoundException extends Exception {
+    public PacienteNotFoundException() {
+        super("Paciente não encontrado");
+    }
 
+    public PacienteNotFoundException(String message) {
+        super(message);
+    }
+
+    public PacienteNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public PacienteNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
