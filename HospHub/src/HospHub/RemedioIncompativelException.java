29b528d024c559aeecb5a4cfa507b8207caf9af8
D-nss <127433077+D-nss@users.noreply.github.com>
2023-06-27 01:09:25 -0300

    Update RemedioIncompativelException.java

--- HospHub/src/HospHub/RemedioIncompativelException.java
+++ HospHub/src/HospHub/RemedioIncompativelException.java
@@ -1,5 +1,19 @@
 package HospHub;
 
 public class RemedioIncompativelException extends Exception {
+    public RemedioIncompativelException() {
+        super("Remédio incnpatível");
+    }
 
+    public RemedioIncompativelException(String message) {
+        super(message);
+    }
+
+    public RemedioIncompativelException(Throwable cause) {
+        super(cause);
+    }
+
+    public RemedioIncompativelException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
